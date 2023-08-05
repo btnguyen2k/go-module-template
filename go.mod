68f78cdfb75ae73e3139f0f3be80d54d0ec8d8c7
Thanh Nguyen <thanhn@microsoft.com>
2023-08-06 00:34:36 +1000

    init project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/btnguyen2k/mymodule
+
+go 1.18
+
+require github.com/btnguyen2k/consu/semver v0.1.1
---
+++ module.go
@@ -0,0 +1,10 @@
+// Package mymodule provides a template for Go modules. Simply clone this GitHub repository and start coding.
+package mymodule
+
+const (
+	// Version holds the semantic version number of this module.
+	Version = "0.1.0"
+)
+
+// This file contains module's metadata only, which is package level documentation and module Version string.
+// Module's code should go into other files.
